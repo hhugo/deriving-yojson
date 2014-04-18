d02ad21fa14a22893e29c10d80e5edd1b9fdf49e
Hugo Heuzard <hugo.heuzard@gmail.com>
2014-04-18 16:59:41 +0200

    update toploop

--- exemple/toploop.ml
+++ exemple/toploop.ml
@@ -1,5 +1,8 @@
 #use "topfind";;
 #camlp4o;;
+#require "deriving.syntax";;
+(* #require "deriving.syntax.tc";; *)
 #require "deriving-yojson.syntax";;
 #require "deriving-yojson";;
-type t = int deriving (Yojson);;
+type t = int deriving(Yojson)
+(* type t = int with yojson;; *)
